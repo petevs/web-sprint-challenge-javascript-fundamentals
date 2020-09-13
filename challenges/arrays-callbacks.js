8fcb1062bbf70ba05ad790f9c550b3a9eee93fd7
Pete VS <pete_vs88@me.com>
2020-09-13 19:19:53 -0400

    arrays-callbacks task 1

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -23,6 +23,10 @@ The zoos want to display both the scientific name and the animal name in front o
 const displayNames = [];
 console.log(displayNames);
 
+zooAnimals.forEach(function(animal){
+  displayNames.push(`Name: ${animal.animal_name}, ${animal.scientific_name}`)
+})
+
 /* Request 2: .map()
 
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
